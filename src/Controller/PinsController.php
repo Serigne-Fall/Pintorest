1bc622522a4cd6386cd16f907d00707074cad7bc
engires <engires1813@gmail.com>
2021-09-14 18:06:38 +0000

    modification de notre entite pin (ajout 2 champs: createdAt UpdatedAt) methode updateTimestamp et affichage des pin par ordre de derniere creation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -7,15 +7,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\PinRepository;
+use App\Entity\Pin;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/pins", name="pins")
+     * @Route("/pins", name="app_home")
      */
     public function index(EntityManagerInterface $em,PinRepository $repo): Response
     {
-        $pins=$repo->findAll(Pin::class);
+        $pins=$repo->findBy([],['createdAt'=>"DESC"]);
         return $this->render('pins/index.html.twig',compact('pins'));
     }
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     */
+    public function show(Pin $pin):Response
+    {
+        return $this->render("pins/show.html.twig",compact("pin"));
+    }
+
 }
