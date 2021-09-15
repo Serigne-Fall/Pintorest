8d9956920df94a599edba65737939dbec6d1d9b2
engires <engires1813@gmail.com>
2021-09-15 19:14:22 +0000

    add form create  et edit form  et un navbar simple

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -8,16 +8,69 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\PinRepository;
 use App\Entity\Pin;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PinsController extends AbstractController
 {
     /**
      * @Route("/pins", name="app_home")
      */
-    public function index(EntityManagerInterface $em,PinRepository $repo): Response
+public function index(EntityManagerInterface $em,PinRepository $repo): Response
     {
         $pins=$repo->findBy([],['createdAt'=>"DESC"]);
         return $this->render('pins/index.html.twig',compact('pins'));
+    }
+
+    /**
+     * @Route("/pins/create", name="app_pins_create")
+     */
+    public function create(Request $req,EntityManagerInterface $em):Response
+    {
+        $pin =new Pin;
+        $form= $this->createFormBuilder($pin)
+        ->add("title",TextType::class)
+        ->add("description",TextareaType::class)
+        ->getForm()
+        ;
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute("app_home");
+        }
+        return $this->render("pins/create.html.twig",[
+            "monFormulaire"=>$form->createView()
+        ]);
+
+    }
+
+
+    /**
+     * @Route("/pins//edit/{id<[0-9]+>}", name="app_pins_edit")
+     */
+    public function edit(Request $req,Pin $pin,EntityManagerInterface $em):Response
+    {
+        $form= $this->createFormBuilder($pin)
+        ->add("title",TextType::class)
+        ->add("description",TextareaType::class)
+        ->getForm()
+        ;
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+
+            return $this->redirectToRoute("app_home");
+        }
+        return $this->render("pins/edit.html.twig",[
+            "monFormulaireEdit"=>$form->createView(),
+            "pin"=>$pin
+        ]);
+
     }
     /**
      * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Entity;
-
+use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -12,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Pin
 {
+    use Timestampable;
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -29,15 +30,7 @@ class Pin
      */
     private $description;
 
-    /**
-     * @ORM\Column(type="datetime_immutable",options={"default":"CURRENT_TIMESTAMP"})
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime_immutable",options={"default":"CURRENT_TIMESTAMP"})
-     */
-    private $updatedAt;
+    
 
     public function getId(): ?int
     {
@@ -68,38 +61,5 @@ class Pin
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-    public function UpdateTimeStamps()
-    {
-        if($this->getCreatedAt()===null){
-            $this->setCreatedAt(new \DateTimeImmutable);
-        }
-        $this->setUpdatedAt(new \DateTimeImmutable);
-    }
+   
 }
