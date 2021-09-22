234e4ed9c14f0ba57d62addaaf51138fb49dec57
engires <engires1813@gmail.com>
2021-09-22 16:18:38 +0000

    add bootstrapp jquery et popper.js

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+//jquery popper.js
+import $ from 'jquery';
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
+import './scss/app.scss';
 // start the Stimulus application
 import './bootstrap';
