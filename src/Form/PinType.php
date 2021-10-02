5f6ac133a1d833a6123cb10e38f1848d230e78a7
engires <engires1813@gmail.com>
2021-10-02 14:35:27 +0000

    coorrection error import for Pin Type

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -7,8 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
-use Vich\UploaderBundle\Form\Type\TypeType;
-use Vich\UploaderBundle\Form\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PinType extends AbstractType
 {
