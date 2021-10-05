5d95c29abca449f78058bf3714e5d116b3d17954
engires <engires1813@gmail.com>
2021-10-05 18:36:08 +0000

    add editformType (sc make:form ..) modification du profile fonctionne bien msaintenant

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccountController extends AbstractController
 {
@@ -15,6 +18,25 @@ class AccountController extends AbstractController
     {
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+
+    /**
+     * @Route("/account/edit", name="app_account_edit")
+     */
+    public function edit(Request $req,EntityManagerInterface $em): Response
+    {
+        $user=$this->getUser();
+        $form = $this->createForm(UserFormType::class,$user);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+             $em->flush();
+             $this->addFlash('success','Profile updated successfully');
+             return $this->redirectToRoute('app_account');
+        }
+        return $this->renderForm('account/edit.html.twig', [
+            'controller_name' => 'AccountController',
+            'form' => $form,
         ]);
     }
 }
