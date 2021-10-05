f23f80c78474e2e811012c745fbb7c14b50c7e63
engires <engires1813@gmail.com>
2021-10-05 18:40:50 +0000

    plus de precission

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -6,15 +6,17 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('email',EmailType::class)
+            ->add('firstName',TextType::class)
+            ->add('lastName',TextType::class)
         ;
     }
 
